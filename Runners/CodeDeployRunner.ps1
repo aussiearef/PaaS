45380e6420902e9be62739858a5af541c35f29cd
Aref Karimi <aref.karimi@just-eat.com>
2018-01-07 09:58:06 +1100

    Added CodeDeployRunner.ps1

--- Runners/CodeDeployRunner.ps1
+++ Runners/CodeDeployRunner.ps1
@@ -1,9 +1,9 @@
 params
 (
 [String] $AppName,
-[String] $S3Bucket=,
+[String] $S3Bucket,
 [String] $DeploymentGroupName,
 [String] $S3LocationKey 
 )
 
-New-CDDeployment $AppName $S3Bucket "zip" $S3LocationKey  "S3" -Force
+New-CDDeployment -ApplicationName $AppName -DeploymentGroupName $DeploymentGroupName  -S3Location_Bucket $S3Bucket -S3Location_BundleType "zip" -S3Location_Key $S3LocationKey  S3LocationKey -Force
