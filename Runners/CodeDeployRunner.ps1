116ef4bb2d61b4fb493262ad7d302f2f22f8ac8c
Aref Karimi <aref.karimi@just-eat.com>
2018-01-07 11:21:14 +1100

    Fixed a bug in S3 Location Key

--- Runners/CodeDeployRunner.ps1
+++ Runners/CodeDeployRunner.ps1
@@ -6,4 +6,4 @@ params
 [String] $S3LocationKey 
 )
 
-New-CDDeployment -ApplicationName $AppName -DeploymentGroupName $DeploymentGroupName  -S3Location_Bucket $S3Bucket -S3Location_BundleType "zip" -S3Location_Key $S3LocationKey  S3LocationKey -Force
+New-CDDeployment -ApplicationName $AppName -DeploymentGroupName $DeploymentGroupName  -S3Location_Bucket $S3Bucket -S3Location_BundleType "zip" -S3Location_Key $S3LocationKey  -Force
