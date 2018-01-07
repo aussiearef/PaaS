9b08488ba5dfd17aeb645f773436f233c73faa51
Aref Karimi <aref.karimi@just-eat.com>
2018-01-07 11:38:23 +1100

    Added parameters for AccessKey and SecretKey to CodeDeployRunner.ps1

--- Runners/CodeDeployRunner.ps1
+++ Runners/CodeDeployRunner.ps1
@@ -3,7 +3,9 @@ param
 [String] $AppName,
 [String] $S3Bucket,
 [String] $DeploymentGroupName,
-[String] $S3LocationKey 
+[String] $S3LocationKey,
+[String] $AccessKey,
+[String] $SecretKey
 )
 
 New-CDDeployment -ApplicationName $AppName -DeploymentGroupName $DeploymentGroupName  -S3Location_Bucket $S3Bucket -S3Location_BundleType "zip" -S3Location_Key $S3LocationKey  -AccessKey $AccessKey  -SecretKey $SecretKey -Force
