002aa18837bd14400ae635dabe028b3520b1f88a
Aref Karimi <aref.karimi@just-eat.com>
2018-01-07 11:47:25 +1100

    Added the missing revision type to the script

--- Runners/CodeDeployRunner.ps1
+++ Runners/CodeDeployRunner.ps1
@@ -8,4 +8,4 @@ param
 [String] $SecretKey
 )
 
-New-CDDeployment -ApplicationName $AppName -DeploymentGroupName $DeploymentGroupName  -S3Location_Bucket $S3Bucket -S3Location_BundleType "zip" -S3Location_Key $S3LocationKey  -AccessKey $AccessKey  -SecretKey $SecretKey -Force
+New-CDDeployment -ApplicationName $AppName -DeploymentGroupName $DeploymentGroupName  -S3Location_Bucket $S3Bucket -S3Location_BundleType "zip" -RevisionType "S3" -S3Location_Key $S3LocationKey  -AccessKey $AccessKey  -SecretKey $SecretKey -Force
